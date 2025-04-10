13bbb1a5012753f188e33a6a2d587ac5a1da73e5
PavlaVitaskova <pavlavitaskova@gmail.com>
2025-04-10 17:17:06 +0200

    překlad konstant do angličtiny a vytvoření funkce, která vykreslí hrací pole

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ email: pavlavitaskova.pv@gmail.com
 discord: pavlavitaskova_29682
 """
 
-DVOJITY_ODDELOVAC = 44 * "="
-JEDNODUCHY_ODDELOVAC = 44 * "-"
+DOUBLE_SEPARATOR = 44 * "="
+SIMPLE_SEPARATOR = 44 * "-"
 
 # Introductory game text
 print(f"""
 Welcome to Tic Tac Toe
-{DVOJITY_ODDELOVAC}
+{DOUBLE_SEPARATOR}
 GAME RULES:
 Each player can place one mark (or stone)
 per turn on the 3x3 grid. The WINNER is
@@ -20,7 +20,19 @@ marks in a:
 * horizontal,
 * vertical or
 * diagonal row
-{DVOJITY_ODDELOVAC}
+{DOUBLE_SEPARATOR}
 Let's start the game
-{JEDNODUCHY_ODDELOVAC}
+{SIMPLE_SEPARATOR}
 """)
+
+# Function to draw the game board
+def draw_the_game_board(game_board):
+    print("-" * 44)
+    print("+---+---+---+")
+    print(f"| {game_board[0]} | {game_board[1]} | {game_board[2]} |")
+    print("+---+---+---+")
+    print(f"| {game_board[3]} | {game_board[4]} | {game_board[5]} |")
+    print("+---+---+---+")
+    print(f"| {game_board[6]} | {game_board[7]} | {game_board[8]} |")
+    print("+---+---+---+")
+    print(DOUBLE_SEPARATOR)
