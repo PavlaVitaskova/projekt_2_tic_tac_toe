790d0ffde2772c899bb693a935d6c573f08c2f6a
PavlaVitaskova <pavlavitaskova@gmail.com>
2025-04-10 16:58:22 +0200

    Introductory game text

--- main.py
+++ main.py
@@ -4,3 +4,23 @@ author: Pavla Vitaskova
 email: pavlavitaskova.pv@gmail.com
 discord: pavlavitaskova_29682
 """
+
+DVOJITY_ODDELOVAC = 44 * "="
+JEDNODUCHY_ODDELOVAC = 44 * "-"
+
+# Introductory game text
+print(f"""
+Welcome to Tic Tac Toe
+{DVOJITY_ODDELOVAC}
+GAME RULES:
+Each player can place one mark (or stone)
+per turn on the 3x3 grid. The WINNER is
+who succeeds in placing three of their
+marks in a:
+* horizontal,
+* vertical or
+* diagonal row
+{DVOJITY_ODDELOVAC}
+Let's start the game
+{JEDNODUCHY_ODDELOVAC}
+""")
